5fdf23135ee5673f9a1131cedab73ff689929c51
Julien Laforge <julien.laforge@gmx.fr>
2019-03-18 17:44:52 +0100

    display the 'see cart' buttons in the welcome page when user is logged in

--- src/app/components/welcome/ad-card/ad-card.component.ts
+++ src/app/components/welcome/ad-card/ad-card.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
+import { UserService } from './../../../services/user.service';
 
 @Component({
   selector: 'app-ad-card',
   templateUrl: './ad-card.component.html',
   styleUrls: ['./ad-card.component.css']
 })
-export class AdCardComponent implements OnInit {
+export class AdCardComponent implements OnInit, DoCheck {
 
+  isLogged = false;
   @Input() ad;
-  constructor() { }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  ngDoCheck() {
+    this.isLogged = this.userService.isLogged();
+  }
 }
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { AdService } from './../../services/ad.service';
 
 @Component({
